27f68fe465354d619c7b37a305135365ac8052b7
Cesar Rivas <crivasg@gmail.com>
2012-10-20 22:24:44 -0500

    Added old app delegate to project

--- TrackerAppDelegate.h
+++ TrackerAppDelegate.h
@@ -2,7 +2,7 @@
 //  TrackerAppDelegate.h
 //  Tracker
 //
-//  Created by Cesar Rivas on 10/20/12.
+//  Created by Cesar Rivas on 9/30/12.
 //  Copyright 2012 __MyCompanyName__. All rights reserved.
 //
 
@@ -10,8 +10,30 @@
 
 @interface TrackerAppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
+	NSSlider *massSlider;
+	NSSlider *weightsSlider;
+	NSSlider *walkingSlider;
+	NSSlider *runningSlider;
+	
+	NSDatePicker *datePicker;
+	
+	NSTabView *tabView;
+	
 }
 
 @property (assign) IBOutlet NSWindow *window;
+
+@property (assign) IBOutlet NSSlider *massSlider;
+@property (assign) IBOutlet NSSlider *weightsSlider;
+@property (assign) IBOutlet NSSlider *walkingSlider;
+@property (assign) IBOutlet NSSlider *runningSlider;
+
+@property (assign) IBOutlet NSDatePicker *datePicker;
+
+@property (assign) IBOutlet NSTabView *tabView;
+
+- (IBAction)store: (id)sender;
+
+- (void)setup;
 
 @end
--- TrackerAppDelegate.m
+++ TrackerAppDelegate.m
@@ -2,7 +2,7 @@
 //  TrackerAppDelegate.m
 //  Tracker
 //
-//  Created by Cesar Rivas on 10/20/12.
+//  Created by Cesar Rivas on 9/30/12.
 //  Copyright 2012 __MyCompanyName__. All rights reserved.
 //
 
@@ -12,8 +12,54 @@
 
 @synthesize window;
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	// Insert code here to initialize your application 
+@synthesize massSlider;
+@synthesize weightsSlider;
+@synthesize walkingSlider;
+@synthesize runningSlider;
+
+@synthesize datePicker;
+
+@synthesize tabView;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification 
+{
+	
+	[self setup];
+
 }
+
+- (BOOL) applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
+{
+	return YES;
+}
+
+- (IBAction)store: (id)sender
+{
+	NSLog(@"hola....");
+	
+	NSLog(@"%@",[ [tabView selectedTabViewItem] label]);
+	
+	NSLog(@"%@",[ datePicker dateValue]);
+}
+
+- (void)setup
+{
+
+	NSDate *today = [ NSDate date];
+	
+	[datePicker setDateValue:today];
+	
+	[massSlider setFloatValue:123.3];
+	[weightsSlider setIntegerValue:50];
+	[walkingSlider setIntegerValue:30];
+	[runningSlider setIntegerValue:0];
+	
+	[massSlider performClick:nil];
+	[weightsSlider performClick:nil];
+	[walkingSlider performClick:nil];
+	[runningSlider performClick:nil];
+
+}
+
 
 @end
